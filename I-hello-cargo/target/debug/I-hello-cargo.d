a5f4a66e54ddb73e38e28605ead69805add164b4
CherifiMi <cherifi154949@gmail.com>
2023-03-23 12:17:28 +0100

    .

---
+++ I-hello-cargo/target/debug/I-hello-cargo.d
@@ -0,0 +1,1 @@
+D:\mito\resources\rust\rust-101\I-hello-cargo\target\debug\I-hello-cargo.exe: D:\mito\resources\rust\rust-101\I-hello-cargo\src\main.rs
---
+++ I-hello-cargo/target/debug/deps/I_hello_cargo.d
@@ -0,0 +1,5 @@
+D:\mito\resources\rust\rust-101\I-hello-cargo\target\debug\deps\I_hello_cargo.exe: src\main.rs
+
+D:\mito\resources\rust\rust-101\I-hello-cargo\target\debug\deps\I_hello_cargo.d: src\main.rs
+
+src\main.rs:
