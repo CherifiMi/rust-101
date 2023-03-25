8b0da0ae45bbffaa7848c832394ada8ed38653ed
CherifiMi <cherifi154949@gmail.com>
2023-03-25 17:06:12 +0100

    .

--- III-consepts/src/main.rs
+++ III-consepts/src/main.rs
@@ -1,7 +1,37 @@
-use std::arch::x86_64::_mm256_madd52hi_epu64;
-
 fn main() {
-    IV()
+    V()
+}
+
+fn V() {
+    // conditions
+    {
+        let number = 3;
+
+        if number < 5 {
+            println!("condition was true");
+        } else {
+            println!("condition was false");
+        }
+
+
+        let condition = true;
+        let number = if condition { 5 } else { 6 };
+
+
+        if number % 4 == 0 {
+            println!("number is divisible by 4");
+        } else if number % 3 == 0 {
+            println!("number is divisible by 3");
+        } else if number % 2 == 0 {
+            println!("number is divisible by 2");
+        } else {
+            println!("number is not divisible by 4, 3, or 2");
+    }
+}
+    // loops
+    {
+
+    }
 }
 
 fn IV() {
