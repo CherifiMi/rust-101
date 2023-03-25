7806361f2a39eea077dc989e411ba7a7edd0c869
CherifiMi <cherifi154949@gmail.com>
2023-03-25 12:27:58 +0100

    .

--- II-gessing-game/src/main.rs
+++ II-gessing-game/src/main.rs
@@ -1,3 +1,33 @@
+use std::cmp::Ordering;
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Guess the number");
+    let rand = 29;
+    loop {
+        let mut geuss = String::new();
+        io::stdin()
+            .read_line(&mut geuss)
+            .expect("fail to read line");
+
+        let geuss: u32 =match geuss
+            .trim()
+            .parse()
+            {
+                Ok(num) => num,
+                Err(_)=>continue,
+            };
+
+        println!("you gessed {geuss}");
+
+        match geuss.cmp(&rand) {
+            Ordering::Less => println!("too small"),
+            Ordering::Greater => println!("too big"),
+            Ordering::Equal => {
+                println!("win");
+                break;
+            },
+        }
+    }
 }
