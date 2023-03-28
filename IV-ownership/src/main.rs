e8ec44d81c1ac3ece1dd6a5c46f497b74ba556ac
CherifiMi <cherifi154949@gmail.com>
2023-03-28 17:34:05 +0100

    .

--- IV-ownership/src/main.rs
+++ IV-ownership/src/main.rs
@@ -95,7 +95,14 @@ fn III() {
 
     let a = [1, 2, 3, 4, 5];
 
+    let slice = &a[1..3];
 
+    for i in &[2, 3] {
+        println!("{ }",i);
+    }
+    for i in slice {
+        println!("{ }",i);
+    }
 }
 
 fn II() {
