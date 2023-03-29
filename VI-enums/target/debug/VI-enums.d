a9454852b360b8b46dfb542e9977ad1729e91521
CherifiMi <cherifi154949@gmail.com>
2023-03-29 01:49:16 +0100

    .

---
+++ VI-enums/target/debug/VI-enums.d
@@ -0,0 +1,1 @@
+D:\mito\resources\rust\rust-101\VI-enums\target\debug\VI-enums.exe: D:\mito\resources\rust\rust-101\VI-enums\src\main.rs
---
+++ VI-enums/target/debug/deps/VI_enums.d
@@ -0,0 +1,5 @@
+D:\mito\resources\rust\rust-101\VI-enums\target\debug\deps\VI_enums.exe: src\main.rs
+
+D:\mito\resources\rust\rust-101\VI-enums\target\debug\deps\VI_enums.d: src\main.rs
+
+src\main.rs:
