5d470aaad9ee43d6804f6cc618cadff676f6d58b
CherifiMi <cherifi154949@gmail.com>
2023-03-31 11:45:33 +0100

    .

--- V-structs/src/main.rs
+++ V-structs/src/main.rs
@@ -3,7 +3,22 @@ fn main() {
 }
 
 fn I() {
+    struct User{
+        active: bool,
+        username: String,
+        gmail: String,
+        sign_in_count: u64,
+    }
 
+    let mut user = User {
+        active: true,
+        username: String::from("mito"),
+        gmail: String::from("hi@gmail.com"),
+        sign_in_count: 3
+    };
 
+    println!("{ }", user.gmail);
+    user.gmail = String::from("hello@gmail.com");
+    println!("{ }", user.gmail);
 
 }
