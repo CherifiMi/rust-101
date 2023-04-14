504d4ac875ccde9948afe6b18161f370af26a9aa
CherifiMi <cherifi154949@gmail.com>
2023-04-14 18:14:45 +0100

    .

--- VI-enums/src/main.rs
+++ VI-enums/src/main.rs
@@ -204,5 +204,11 @@ fn II(){
     }
 }
 fn III(){
+    let config_max = Some(3u8);
+    match config_max {
+        Some(max) => println!("{}",max),
+        _ => ()
+    }
 
+    if let Some(max) = config_max{println!("{}",max)} else {  }
 }
