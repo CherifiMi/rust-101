ca10d234b9f4bfb0664d8a44bd1e9197c4595711
CherifiMi <cherifi154949@gmail.com>
2023-05-14 18:25:59 +0100

    .

---
+++ VII-crates-modules/target/debug/VII-crates-modules.d
@@ -0,0 +1,1 @@
+D:\mito\resources\rust\rust-101\VII-crates-modules\target\debug\VII-crates-modules.exe: D:\mito\resources\rust\rust-101\VII-crates-modules\src\main.rs
---
+++ VII-crates-modules/target/debug/deps/VII_crates_modules.d
@@ -0,0 +1,5 @@
+D:\mito\resources\rust\rust-101\VII-crates-modules\target\debug\deps\VII_crates_modules.exe: src\main.rs
+
+D:\mito\resources\rust\rust-101\VII-crates-modules\target\debug\deps\VII_crates_modules.d: src\main.rs
+
+src\main.rs:
