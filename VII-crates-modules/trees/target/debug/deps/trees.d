e67176e5eddbfe9cd06d578783c8a74e2769c245
CherifiMi <cherifi154949@gmail.com>
2023-05-15 05:04:04 +0100

    .

---
+++ VII-crates-modules/trees/target/debug/deps/trees.d
@@ -0,0 +1,5 @@
+D:\mito\resources\rust\rust-101\VII-crates-modules\trees\target\debug\deps\trees.exe: src\main.rs
+
+D:\mito\resources\rust\rust-101\VII-crates-modules\trees\target\debug\deps\trees.d: src\main.rs
+
+src\main.rs:
