66a178cc876ce93be5fc327bade074356d39a970
CherifiMi <cherifi154949@gmail.com>
2023-05-15 16:00:06 +0100

    .

--- VIII-collections/src/main.rs
+++ VIII-collections/src/main.rs
@@ -2,7 +2,29 @@ use std::arch::x86_64::_blci_u32;
 use std::collections::HashMap;
 
 fn main() {
-    ex1()
+    ex2()
+}
+
+//region ex
+/*
+- ex1: Given a list of integers, use a vector and return the median (when sorted, the value in
+    the middle position) and mode (the value that occurs most often;
+    a hash map will be helpful here) of the list.
+
+- ex2: Convert strings to pig latin. The first consonant of each word
+    is moved to the end of the word and “ay” is added, so “first” becomes “irst-fay.”
+    Words that start with a vowel have “hay” added to the end instead (“apple” becomes “apple-hay”).
+    Keep in mind the details about UTF-8 encoding!
+
+- ex3: Using a hash map and vectors, create a text interface to
+    allow a user to add employee names to a department in a company.
+     For example, “Add Sally to Engineering” or “Add Amir to Sales.”
+     Then let the user retrieve a list of all people in a department or all people in
+     the company by department, sorted alphabetically.
+*/
+
+fn ex2() {
+
 }
 
 fn ex1() {
@@ -27,6 +49,7 @@ fn ex1() {
     dbg!(median);
     dbg!(mode);
 }
+//endregion
 
 fn HashMaps() {
     let mut scores = HashMap::new();
