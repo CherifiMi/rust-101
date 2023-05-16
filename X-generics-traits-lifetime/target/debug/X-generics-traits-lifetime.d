eec8426353ec3279163bad59c9b9f72af976e8b6
CherifiMi <cherifi154949@gmail.com>
2023-05-16 17:07:32 +0100

    .

---
+++ X-generics-traits-lifetime/target/debug/X-generics-traits-lifetime.d
@@ -0,0 +1,1 @@
+D:\mito\resources\rust\rust-101\X-generics-traits-lifetime\target\debug\X-generics-traits-lifetime.exe: D:\mito\resources\rust\rust-101\X-generics-traits-lifetime\src\main.rs
---
+++ X-generics-traits-lifetime/target/debug/deps/X_generics_traits_lifetime.d
@@ -0,0 +1,5 @@
+D:\mito\resources\rust\rust-101\X-generics-traits-lifetime\target\debug\deps\X_generics_traits_lifetime.exe: src\main.rs
+
+D:\mito\resources\rust\rust-101\X-generics-traits-lifetime\target\debug\deps\X_generics_traits_lifetime.d: src\main.rs
+
+src\main.rs:
