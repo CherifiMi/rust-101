6d6312c03d0f1f207bce249ce612ca9ca3c986be
CherifiMi <cherifi154949@gmail.com>
2023-05-16 14:55:29 +0100

    .

--- IX-errors/src/main.rs
+++ IX-errors/src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::{Error, ErrorKind, Read};
-use std::str;
+use std::{fs, str};
 
 fn main() {
     err_propagating();
@@ -8,15 +8,43 @@ fn main() {
 
 fn err_propagating() {
 
-    match read_username2() {
-        Ok(a) => {
-            println!("{}", a );
+    let text = "my name is boox\n
+                my fav is patato \n
+                yoo";
+
+    fn last_char_of_first_line(text: &str) -> Option<char>{
+        text.lines().next()?.chars().last()
+    }
+
+    match last_char_of_first_line(text) {
+        Some(s) => {
+            println!("last character is{}", s);
+
         }
-        Err(e) => {
-            println!("{}",e);
+        None => {
+            println!("there is nothing");
         }
     }
 
+    //------------
+    match read_username4() {
+        Ok(a) => {
+            println!("file is full with {}", a );
+        }
+        Err(e) => {
+            println!("######error is {}",e);
+        }
+    }
+
+    fn read_username4() -> Result<String, Error> {
+        fs::read_to_string("hi.txt")
+    }
+    fn read_username3() -> Result<String, Error> {
+        let mut username = String::new();
+        File::open("hi.txt")?.read_to_string(&mut username)?;
+
+        Ok(username)
+    }
     fn read_username2() -> Result<String, Error> {
         let mut username_file = File::open("hi.txt")?;
         let mut username = String::new();
