87cf87b3cb7b997726381bf77a9d552931e78fd3
CherifiMi <cherifi154949@gmail.com>
2023-05-16 11:50:47 +0100

    .

--- IX-errors/src/main.rs
+++ IX-errors/src/main.rs
@@ -1,7 +1,25 @@
+use std::fs::File;
+use std::io::{Error, ErrorKind};
 use std::str;
 
 fn main() {
     println!("Hello, world!");
 
-    //panic!("run away");
+    let file = File::open("hi.txt");
+    let file = match file {
+        Ok(file) => {file}
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => {
+                match File::create("hi.txt") {
+                    Ok(file) => {file}
+                    Err(error) => {
+                        panic!("no file{:?}", error);
+                    }
+                }
+            }
+            _ => {
+                panic!("no file{:?}", error);
+            }
+        }
+    };
 }
