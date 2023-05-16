d833baca2c5dc39d32a421a6a4edd5d120606b06
CherifiMi <cherifi154949@gmail.com>
2023-05-16 14:18:29 +0100

    .

--- IX-errors/src/main.rs
+++ IX-errors/src/main.rs
@@ -1,10 +1,39 @@
 use std::fs::File;
-use std::io::{Error, ErrorKind};
+use std::io::{Error, ErrorKind, Read};
 use std::str;
 
 fn main() {
-    println!("Hello, world!");
+    err_propagating();
+}
 
+fn err_propagating() {
+
+    match read_username() {
+        Ok(a) => {
+            println!("{}", a );
+        }
+        Err(e) => {
+            println!("{}",e);
+        }
+    }
+
+    fn read_username() -> Result<String, Error> {
+        let username_file_result = File::open("hi.txt");
+        let mut username_file = match username_file_result {
+            Ok(file) => { file }
+            Err(e) => return Err(e),
+        };
+
+        let mut username = String::new();
+
+        match username_file.read_to_string(&mut username) {
+            Ok(_) => Ok(username),
+            Err(e) => Err(e)
+        }
+    }
+}
+
+fn err() {
     let file = File::open("hillo.txt");
     let file = match file {
         Ok(file) => {file}
@@ -22,11 +51,12 @@ fn main() {
 
     //unwrap panics if not found
     let file2 =
-        File::open("mito.txt").unwrap();
+        File::open("hillo.txt").unwrap();
 
     // expect same as unwrap, dose not use default err panic message
     let file2 =
-        File::open("mito.txt").expect("smt wrong happened");
+        File::open("hillo.txt").expect("smt wrong happened");
 
     dbg!(file2);
+
 }
