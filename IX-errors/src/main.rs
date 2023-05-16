25e5dffe29dda441dca1a5eb7c9a9a3efec2e14b
CherifiMi <cherifi154949@gmail.com>
2023-05-16 13:35:56 +0100

    .

--- IX-errors/src/main.rs
+++ IX-errors/src/main.rs
@@ -5,21 +5,28 @@ use std::str;
 fn main() {
     println!("Hello, world!");
 
-    let file = File::open("hi.txt");
+    let file = File::open("hillo.txt");
     let file = match file {
         Ok(file) => {file}
         Err(error) => match error.kind() {
             ErrorKind::NotFound => {
-                match File::create("hi.txt") {
-                    Ok(file) => {file}
-                    Err(error) => {
-                        panic!("no file{:?}", error);
-                    }
-                }
+                File::create("hillo.txt").unwrap_or_else(|error|{
+                    panic!("no file{:?}", error);
+                })
             }
             _ => {
                 panic!("no file{:?}", error);
             }
         }
     };
+
+    //unwrap panics if not found
+    let file2 =
+        File::open("mito.txt").unwrap();
+
+    // expect same as unwrap, dose not use default err panic message
+    let file2 =
+        File::open("mito.txt").expect("smt wrong happened");
+
+    dbg!(file2);
 }
