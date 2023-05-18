9ca5210e8ab54091a89810328f998d991e07446a
CherifiMi <cherifi154949@gmail.com>
2023-05-18 16:33:57 +0100

    .

---
+++ XII-cmd-program/src/lib.rs
@@ -0,0 +1,30 @@
+use std::error::Error;
+use std::fs;
+
+pub struct Config {
+    query: String,
+    file_path: String,
+}
+
+
+impl Config {
+    pub fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough argements");
+        }
+
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Ok(Config { query, file_path })
+    }
+    pub fn run(self) -> Result<(), Box<dyn Error>>{
+        let content = fs::read_to_string(&self.file_path)?;
+
+        println!("Searching for {}", self.query);
+        println!("In file {}", self.file_path);
+        println!("{content}");
+
+        Ok(())
+    }
+}
+
--- XII-cmd-program/src/main.rs
+++ XII-cmd-program/src/main.rs
@@ -1,5 +1,6 @@
 use std::{env, fs, process};
 use std::error::Error;
+use XII_cmd_program::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -13,35 +14,4 @@ fn main() {
         println!("problem running: {}", e);
         process::exit(1);
     }
-    /*config.run().unwrap_or_else(|err|{
-        println!("problem running: {}", err);
-        process::exit(1);
-    });;*/
 }
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough argements");
-        }
-
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config { query, file_path })
-    }
-    fn run(self)-> Result<(), Box<dyn Error>>{
-        let content = fs::read_to_string(&self.file_path)?;
-
-        println!("Searching for {}", self.query);
-        println!("In file {}", self.file_path);
-        println!("{content}");
-
-        Ok(())
-    }
-}
-
