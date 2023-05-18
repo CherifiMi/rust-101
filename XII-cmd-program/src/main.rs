af9af6faecc7d4a7e6248828acb783d652295043
CherifiMi <cherifi154949@gmail.com>
2023-05-18 14:38:11 +0100

    .

--- XII-cmd-program/src/main.rs
+++ XII-cmd-program/src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+
+    let args: Vec<String> = env::args().collect();
+
+    let query = &args[1];
+    let file_path = &args[2];
+
+    let content = fs::read_to_string(file_path)
+        .expect("should have been able to read the text");
+
+    println!("\nsearching for: {}", query);
+    println!("if file: {}", file_path);
+    println!("with text:\n{}", content);
 }
