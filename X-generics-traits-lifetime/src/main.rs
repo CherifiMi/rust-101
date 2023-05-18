94b45b0dd8f001a19ebaf18c9825dc79fa293847
CherifiMi <cherifi154949@gmail.com>
2023-05-18 04:09:41 +0100

    .

--- X-generics-traits-lifetime/src/main.rs
+++ X-generics-traits-lifetime/src/main.rs
@@ -7,6 +7,44 @@ fn main()
 
 fn IV() {
 
+    fn longest<'a>(x: &'a str, y: &'a str)-> &'a str{
+        if x.len()>y.len() {
+            x
+        } else {
+            y
+        }
+    }
+
+    fn main() {
+        let string1 = String::from("abcd");
+        let string2 = "xyz";
+
+        let result = longest_with_an_announcement(
+            string1.as_str(),
+            string2,
+            "Today is someone's birthday!",
+        );
+        println!("The longest string is {}", result);
+    }
+
+    use std::fmt::Display;
+
+    fn longest_with_an_announcement<'a, T>(
+        x: &'a str,
+        y: &'a str,
+        ann: T,
+    ) -> &'a str
+        where
+            T: Display,
+    {
+        println!("Announcement! {}", ann);
+        if x.len() > y.len() {
+            x
+        } else {
+            y
+        }
+    }
+
 }
 
 fn III() {
