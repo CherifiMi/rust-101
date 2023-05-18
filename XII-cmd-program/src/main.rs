7606e37e6817ad8ce0b7b25b3b64e247309b9dd0
CherifiMi <cherifi154949@gmail.com>
2023-05-18 14:52:36 +0100

    .

--- XII-cmd-program/src/main.rs
+++ XII-cmd-program/src/main.rs
@@ -1,25 +1,26 @@
 use std::{env, fs};
 
 fn main() {
-
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config = Config::new(&args);
 
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
     let content = fs::read_to_string(config.file_path)
         .expect("should have been able to read the text");
-
 }
-struct Config{
+
+struct Config {
     query: String,
-    file_path: String
+    file_path: String,
 }
-fn parse_config(args: &[String]) -> Config{
-    let query = args[1].clone();
-    let file_path = args[2].clone();
 
-    Config{ query, file_path }
+impl Config {
+    fn new(args: &[String]) -> Config{
+        let query = args[1].clone();
+        let file_path = args[2].clone();
+        Config{ query, file_path }
+    }
 }
