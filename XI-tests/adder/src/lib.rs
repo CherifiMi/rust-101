8c66a230d5bb51de3070d3c53ac8e098cf6d7f2c
CherifiMi <cherifi154949@gmail.com>
2023-05-18 04:34:27 +0100

    .

--- XI-tests/adder/src/lib.rs
+++ XI-tests/adder/src/lib.rs
@@ -1,3 +1,17 @@
+extern crate core;
+
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
 pub fn add(left: usize, right: usize) -> usize {
     left + right
 }
@@ -7,8 +21,13 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn exploration() {
         let result = add(2, 2);
         assert_eq!(result, 4);
+    }
+
+    #[test]
+    fn another(){
+        panic!("it failed")
     }
 }
