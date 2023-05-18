f96beba40335a26a00092376b6b3cdb9418d8f54
CherifiMi <cherifi154949@gmail.com>
2023-05-18 14:59:04 +0100

    .

--- XII-cmd-program/src/main.rs
+++ XII-cmd-program/src/main.rs
@@ -3,8 +3,10 @@ use std::{env, fs};
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = Config::new(&args);
-
+    let config = match Config::build(&args) {
+        Ok(item) => { item }
+        Err(err) => { println!("{}", err) }
+    };
 
     println!("Searching for {}", config.query);
     println!("In file {}", config.file_path);
@@ -18,13 +20,13 @@ struct Config {
 }
 
 impl Config {
-    fn new(args: &[String]) -> Config{
-        if args.len()<3 {
-            panic!("not enough arguments")
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough argements");
         }
 
         let query = args[1].clone();
         let file_path = args[2].clone();
-        Config{ query, file_path }
+        Ok(Config { query, file_path })
     }
 }
