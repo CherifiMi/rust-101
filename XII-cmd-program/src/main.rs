e125c3904eb4c630783a4739df16eb0672d5187e
CherifiMi <cherifi154949@gmail.com>
2023-05-18 14:49:55 +0100

    .

--- XII-cmd-program/src/main.rs
+++ XII-cmd-program/src/main.rs
@@ -4,13 +4,22 @@ fn main() {
 
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    let content = fs::read_to_string(file_path)
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.file_path);
+    let content = fs::read_to_string(config.file_path)
         .expect("should have been able to read the text");
 
-    println!("\nsearching for: {}", query);
-    println!("if file: {}", file_path);
-    println!("with text:\n{}", content);
+}
+struct Config{
+    query: String,
+    file_path: String
+}
+fn parse_config(args: &[String]) -> Config{
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config{ query, file_path }
 }
