60e15fbfe855245d14c188504e7665e9a17c6515
CherifiMi <cherifi154949@gmail.com>
2023-05-19 21:06:26 +0100

    .

--- XII-cmd-program/target/debug/deps/XII_cmd_program-2d4367d15052a8b7.d
+++ XII-cmd-program/target/debug/deps/XII_cmd_program-2d4367d15052a8b7.d
@@ -1,5 +1,6 @@
-D:\mito\resources\rust\rust-101\XII-cmd-program\target\debug\deps\XII_cmd_program-2d4367d15052a8b7.exe: src\lib.rs
+D:\mito\resources\rust\rust-101\XII-cmd-program\target\debug\deps\XII_cmd_program-2d4367d15052a8b7.exe: src\lib.rs src\tests.rs
 
-D:\mito\resources\rust\rust-101\XII-cmd-program\target\debug\deps\XII_cmd_program-2d4367d15052a8b7.d: src\lib.rs
+D:\mito\resources\rust\rust-101\XII-cmd-program\target\debug\deps\XII_cmd_program-2d4367d15052a8b7.d: src\lib.rs src\tests.rs
 
 src\lib.rs:
+src\tests.rs:
