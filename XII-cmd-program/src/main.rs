5cba13e891110c67fae2b049047fca503f776777
CherifiMi <cherifi154949@gmail.com>
2023-05-20 11:38:04 +0100

    .

--- XII-cmd-program/src/main.rs
+++ XII-cmd-program/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config =Config::build(&args).unwrap_or_else(|err|{
-        println!("problem parsing args: {}", err);
+        eprintln!("problem parsing args: {}", err);
         process::exit(1);
     });
 
     if let Err(e) = config.run(){
-        println!("problem running: {}", e);
+        eprintln!("problem running: {}", e);
         process::exit(1);
     }
 }
