db87d85a09065851f5b3270522a389986cb03e52
CherifiMi <cherifi154949@gmail.com>
2023-05-22 18:41:27 +0100

    .

---
+++ XIII-iterators-closures/target/debug/XIII-iterators-closures.d
@@ -0,0 +1,1 @@
+D:\mito\resources\rust\rust-101\XIII-iterators-closures\target\debug\XIII-iterators-closures.exe: D:\mito\resources\rust\rust-101\XIII-iterators-closures\src\main.rs
---
+++ XIII-iterators-closures/target/debug/deps/XIII_iterators_closures.d
@@ -0,0 +1,5 @@
+D:\mito\resources\rust\rust-101\XIII-iterators-closures\target\debug\deps\XIII_iterators_closures.exe: src\main.rs
+
+D:\mito\resources\rust\rust-101\XIII-iterators-closures\target\debug\deps\XIII_iterators_closures.d: src\main.rs
+
+src\main.rs:
