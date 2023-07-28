7f56301083857a9492717d2dd08c8b41d92cbd0c
CherifiMi <cherifi154949@gmail.com>
2023-07-28 17:51:38 +0100

    .

--- XIII-iterators-closures/src/main.rs
+++ XIII-iterators-closures/src/main.rs
@@ -1,18 +1,48 @@
 use std::collections::HashMap;
+use std::hash::Hash;
 use std::ops::Add;
 use std::ptr::hash;
 use std::thread;
 use std::time::Duration;
 
 fn main() {
+
+    let mut map = HashMap::new();
+
+    map.insert(3, "fizz");
+    map.insert(5, "buzz");
+    map.insert(7, "mopp");
+
+    for i in 1..1000 {
+        let mut result = String::new();
+        for x in map.keys() {
+            if i%x == 0 {
+                result.push_str(map[x])
+            }
+        }
+        if result.is_empty() {
+            result.push_str(i.to_string().as_str())
+        }
+        println!("{}", result)
+    }
+
     //IV()
 }
 
+fn IV() {
+    let v1 = vec![1,2,3];
+    let mut v1_iter = v1.iter();
+
+
+    assert_eq!(v1_iter.next(), Some(&1));
+    assert_eq!(v1_iter.next(), Some(&2));
+    assert_eq!(v1_iter.next(), Some(&3));
+    assert_eq!(v1_iter.next(), None)
+}
 
 fn III() {
-
     /// doesnt borrow
-    /// ```kotlin
+    /// ```rust
     /// let list = vec![1,2,3];
     /// println!("before closure: {:?}", list);
     ///
@@ -22,27 +52,24 @@ fn III() {
     /// only_borrow();
     /// println!("before closure: {:?}", list);
     /// ```
-
     /// mut closure
-    /// ```kotlin
+    /// ```rust
     /// let mut list = vec![1,2,3];
     /// println!("before closure: {:?}", list);
     /// let mut borrows_mutably  = || list.push(5);
     /// borrows_mutably();
     /// println!("before closure: {:?}", list);
     /// ```
-
-    let list = vec![1,2,3];
+    let list = vec![1, 2, 3];
     println!("before closure: {:?}", list);
 
     thread::spawn(move || println!("from thread: {:?}", list))
         .join()
         .unwrap();
-
 }
 
 fn II() {
-    let expansive_closure = |num: u32| -> u32{
+    let expansive_closure = |num: u32| -> u32 {
         println!("calcilating slowly...");
         thread::sleep(Duration::from_secs(2));
         num
@@ -51,8 +78,7 @@ fn II() {
     //fn add_one1(x:u32) -> u32 { x + 1 }
     //let add_one2 = |x:u32| -> u32 { x + 1 };
     //let add_one3 = |x| { x + 1 };
-    let add_one4 = |x| x+1;
-
+    let add_one4 = |x| x + 1;
 
     let r_c = |x| x;
 
@@ -63,15 +89,17 @@ fn II() {
 }
 
 fn I() {
-    let store = Inventory { shirts: vec![ShirtColor::Blue, ShirtColor::Blue, ShirtColor::Red] };
+    let store = Inventory {
+        shirts: vec![ShirtColor::Blue, ShirtColor::Blue, ShirtColor::Red],
+    };
 
     let user_pref1 = Some(ShirtColor::Red);
     let giveaway1 = store.giveaway(user_pref1);
-    println!("the user with pref {:?} gets {:?}",user_pref1, giveaway1);
+    println!("the user with pref {:?} gets {:?}", user_pref1, giveaway1);
 
     let user_pref2 = Some(ShirtColor::Blue);
     let giveaway2 = store.giveaway(user_pref2);
-    println!("the user2 with pref {:?} gets {:?}",user_pref2, giveaway2);
+    println!("the user2 with pref {:?} gets {:?}", user_pref2, giveaway2);
 }
 
 // region closure 1
@@ -84,6 +112,7 @@ enum ShirtColor {
 struct Inventory {
     shirts: Vec<ShirtColor>,
 }
+
 impl Inventory {
     fn giveaway(&self, user_preference: Option<ShirtColor>) -> ShirtColor {
         user_preference.unwrap_or_else(|| self.most_stocked())
