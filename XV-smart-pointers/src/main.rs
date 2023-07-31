68638662050e4e2bf1b541601bbfbeeeb90e2db0
CherifiMi <cherifi154949@gmail.com>
2023-07-31 06:44:21 +0100

    .

--- XV-smart-pointers/src/main.rs
+++ XV-smart-pointers/src/main.rs
@@ -1,15 +1,14 @@
-use crate::List::{Cons, Nil};
-
 fn main() {
     I()
 }
 
 fn I(){
-    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    let x = 5;
+    let y = Box::new(x);
+
+    assert_eq!(5, x);
+    assert_eq!(5, *y);
 
 }
 
-enum List{
-    Cons(i32, Box<List>),
-    Nil
-}
+
