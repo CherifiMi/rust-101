2e958f708817df33b2b0ab627f509ad290a77b2a
CherifiMi <cherifi154949@gmail.com>
2023-07-31 06:42:05 +0100

    .

---
+++ XV-smart-pointers/target/debug/XV-smart-pointers.d
@@ -0,0 +1,1 @@
+D:\code\rust\rust-101\XV-smart-pointers\target\debug\XV-smart-pointers.exe: D:\code\rust\rust-101\XV-smart-pointers\src\main.rs
---
+++ XV-smart-pointers/target/debug/deps/XV_smart_pointers.d
@@ -0,0 +1,5 @@
+D:\code\rust\rust-101\XV-smart-pointers\target\debug\deps\XV_smart_pointers.exe: src\main.rs
+
+D:\code\rust\rust-101\XV-smart-pointers\target\debug\deps\XV_smart_pointers.d: src\main.rs
+
+src\main.rs:
