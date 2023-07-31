d5756d96c364099eedc1df1d879251aa6a8cc7d5
CherifiMi <cherifi154949@gmail.com>
2023-08-01 00:58:25 +0100

    .

--- XV-smart-pointers/src/main.rs
+++ XV-smart-pointers/src/main.rs
@@ -1,14 +1,27 @@
+use std::ops::Deref;
+
 fn main() {
-    I()
+    let m = MyBox::new(String::from("Rust"));
+    hello(&m);
 }
 
-fn I(){
-    let x = 5;
-    let y = Box::new(x);
-
-    assert_eq!(5, x);
-    assert_eq!(5, *y);
-
+fn hello(name: &str) {
+    println!("Hello, {name}!");
 }
 
 
+struct MyBox<T>(T);
+
+impl <T> MyBox<T>{
+    fn new(x: T) -> MyBox<T>{
+        MyBox(x)
+    }
+}
+
+impl <T> Deref for MyBox<T>{
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
